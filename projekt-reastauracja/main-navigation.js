e2f5d6e01186d173fa367035f565f0ad02437c1e
Nataliya Soyko <nataliya.soyko@gmail.com>
2020-04-13 11:48:40 +0200

    poprawione menu

--- projekt-reastauracja/main-navigation.js
+++ projekt-reastauracja/main-navigation.js
@@ -1,9 +1,9 @@
-(()=>{
+(() => {
     const button = document.querySelector("js-main-navigation__mobile-button");
-    const menu = document.querySelector("js-main-navigation-menu");
+    const menu = document.querySelector("js-main-navigation__menu");
 
     const toggleClass = () => {
-       menu.classList.toggle(".main-navigation__menu--open");
+        menu.classList.toggle(".main-navigation__menu--open");
     };
     button.addEventListener("click", toggleClass);
     menu.addEventListener("click", toggleClass);
