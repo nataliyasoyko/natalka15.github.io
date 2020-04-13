1aaedc278ac8d94cebca21450c8632fcfff248e5
Nataliya Soyko <nataliya.soyko@gmail.com>
2020-04-13 11:41:49 +0200

    menu ready

---
+++ projekt-reastauracja/main-navigation.js
@@ -0,0 +1,10 @@
+(()=>{
+    const button = document.querySelector("js-main-navigation__mobile-button");
+    const menu = document.querySelector("js-main-navigation-menu");
+
+    const toggleClass = () => {
+       menu.classList.toggle(".main-navigation__menu--open");
+    };
+    button.addEventListener("click", toggleClass);
+    menu.addEventListener("click", toggleClass);
+})();
