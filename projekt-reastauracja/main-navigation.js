5704444d75e411b9dc3c5cf673c08c21c242e6ff
Nataliya Soyko <nataliya.soyko@gmail.com>
2020-04-15 18:53:14 +0200

    navi

--- projekt-reastauracja/main-navigation.js
+++ projekt-reastauracja/main-navigation.js
@@ -1,9 +1,10 @@
 (() => {
-    const button = document.querySelector("js-main-navigation__mobile-button");
-    const menu = document.querySelector("js-main-navigation__menu");
+    const button = document.querySelector(".js-main-navigation__mobile-button");
+    const menu = document.querySelector(".js-main-navigation__menu");
+    console.log(button)
 
     const toggleClass = () => {
-        menu.classList.toggle(".main-navigation__menu--open");
+        menu.classList.toggle("main-navigation__menu--open");
     };
     button.addEventListener("click", toggleClass);
     menu.addEventListener("click", toggleClass);
